9f4edc9491bd406d893a1fe9b3bf49c4ba506d5d
Mark_Ma <50102783+MarkMaMsw@users.noreply.github.com>
2020-03-15 22:33:11 +1100

    pname_in & pname_out
    write pname_in and pname_out function

--- pname.c
+++ pname.c
@@ -1,30 +1,36 @@
 /*
  * pname.c
- * author: Yinghong Zhong(z5233608)
- *         Shaowei Ma 
+ * author1: Yinghong Zhong(z5233608)
+ * author2: Shaowei Ma(z5238010)
  * version:1.0
+ * course: COMP9315
+ * Item: Assignment1
 */
-
-#include "postgres.h"
-
-#include "fmgr.h"
-#include "libpq/pqformat.h"		/* needed for send/recv functions */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <regex.h>
+#include <string.h>
+
+#include "postgres.h"
+#include "fmgr.h"
+#include "libpq/pqformat.h"		/* needed for send/recv functions */
+#include "c.h"  // include felxable memory size
 
 PG_MODULE_MAGIC;
 
 typedef struct pName
 {
     int length;
-	char *pname;
-}PersonName;
+	// can I use poniter here
+	// or familyName[] ?
+	char familyName[FLEXIBLE_ARRAY_MEMBER];
+	char givenName[FLEXIBLE_ARRAY_MEMBER];
+} pName;
 
 static int checkName(char *name);
 
-static int compareNames(PersonName *a, PersonName *b);
+static int compareNames(pName *a, pName *b);
 
 static bool checkName(char *name){
 	int cflags = REG_EXTENDED;
@@ -43,6 +49,15 @@ static bool checkName(char *name){
 	return ret;
 }
 
+// remove the first space of a name if its first char is a space
+static char *removeFirstSpace(char *name){
+	char *new_name = name;
+	if (*name == ' '){
+		new_name++;
+	}
+	return new_name;
+}
+
 /*****************************************************************************
  * Input/Output functions
  *****************************************************************************/
@@ -52,36 +67,54 @@ PG_FUNCTION_INFO_V1(pname_in);
 Datum
 pname_in(PG_FUNCTION_ARGS)
 {
-	char *pName = PG_GETARG_CSTRING(0);
-	char *familyName, *givenName;
-	PersonName *result;
+	char *NameIn = PG_GETARG_CSTRING(0);
+	char *familyName, 
+		 *givenName;
 
-	if (checkName(pname) == false){
+	if (checkName(NameIn) == false){
 		ereport(ERROR,
 				(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
-				 errmsg("invalid input syntax for type PersonName: \"%s\"", pName)));
+				 errmsg("invalid input syntax for type pname: \"%s\"", NameIn)));
 	}
 
-	givenName = strchr(pName, ',') + 1;
+	// scan the input string and set familyName, givenName
+	sscanf(NameIn, "%s,%s", familyName, givenName);
 
-//here---------------------------------------------------------------------------------------------------------------------------
-/*
-	result = (Complex *) palloc(sizeof(Complex));
-	result->x = x;
-	result->y = y;
-	*/
+	// check if the given name start with a space
+	givenName = removeFirstSpace(givenName);
+
+	// set new pname object's size , +2 for 2"\0"
+	int32 new_pname_size = strlen(familyName) + strlen(givenName) + 2;
+
+	// assign memory to result
+	pName *result = (pName *)palloc(VARHDRSZ + new_pname_size);
+
+	// set VARSIZE
+	SET_VARSIZE(result, VARHDRSZ + new_pname_size);
+
+	// memcopy
+	memcpy(VARDATA(result), VARDATA_ANY(familyName), (strlen(familyName) + 1));
+	memcpy(VARDATA(result) + strlen(familyName) + 1, VARDATA_ANY(givenName), (strlen(givenName) + 1));
+
+	// locate pointer
+	result->familyName = result + VARHDRSZ；
+	result->givenName = result->familyName + strlen(familyName) + 1；
+	
+	// copy familyName and givenName to result
+	//strcpy(result->familyName, familyName);
+	//strcpy(result->givenName, givenName);
 	PG_RETURN_POINTER(result);
 }
 
-PG_FUNCTION_INFO_V1(complex_out);
+PG_FUNCTION_INFO_V1(pname_out);
 
 Datum
-complex_out(PG_FUNCTION_ARGS)
+pname_out(PG_FUNCTION_ARGS)
 {
-	Complex    *complex = (Complex *) PG_GETARG_POINTER(0);
-	char	   *result;
+	pName *fullname = (pName *) PG_GETARG_POINTER(0);
+	char  *result;
 
-	result = psprintf("(%g,%g)", complex->x, complex->y);
+	result = psprintf("(%s,%s)", fullname->familyName, fullname->givenName);
 	PG_RETURN_CSTRING(result);
 }
 
